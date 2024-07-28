6ae4128f30eef1e54d7d5c2a0297ba4c9b6ecfe3
JorikDura <vandarkslave@outlook.com>
2024-07-28 17:21:38 +0300

    add tag see

--- tests/Feature/TagTest.php
+++ tests/Feature/TagTest.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Models\Tag;
+
 use function Pest\Laravel\getJson;
 
 describe('tag tests', function () {
     it('get tags', function () {
+        Tag::factory()->create([
+            'name' => 'testing!'
+        ]);
+
         getJson('/api/v1/tags/')
-            ->assertSuccessful();
+            ->assertSuccessful()
+            ->assertSee('testing!');
     });
 });
