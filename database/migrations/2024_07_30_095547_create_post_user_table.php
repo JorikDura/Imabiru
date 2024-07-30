5e0341cbc8335dd90d48190b9c40b89c75084d78
JorikDura <vandarkslave@outlook.com>
2024-07-30 18:58:55 +0300

    removed cascade

--- database/migrations/2024_07_30_095547_create_post_user_table.php
+++ database/migrations/2024_07_30_095547_create_post_user_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     {
         Schema::create('post_user', function (Blueprint $table) {
             $table->foreignId('post_id')->constrained()->cascadeOnDelete();
-            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('user_id');
         });
     }
 
